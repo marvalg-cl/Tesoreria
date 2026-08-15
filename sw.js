dd32f89c5bf59d0f9613ef5df3ea565a94abceae
marvalg-cl <marvalg@gmail.com>
2026-08-14 21:06:54 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'tesoreria-cache-v1';
+const CACHE_NAME = 'tesoreria-cache-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-512-maskable.png'
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png'
 ];
 
 self.addEventListener('install', (event) => {
