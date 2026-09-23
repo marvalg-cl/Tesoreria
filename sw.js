b77c274aa7bad298dc7d1541c629e6dd82762907
marvalg-cl <marvalg@gmail.com>
2026-09-23 11:53:02 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='tesoreria-pro-v22-reimaginada-2';
-const ASSETS=['./','./index.html','./manifest.json','./favicon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim().then(()=>caches.keys()).then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
